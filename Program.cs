38bb71c0a3954171901b598782806ae6b21459bd
Timur Magomedov <ser4564@gmail.com>
2019-06-09 17:18:34 +0300

    My first game

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game2048
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] map = StartGame();
+            
+        }
+
+        private static int[,] StartGame()
+        {
+            int[,] map = new int[4, 4];
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
+                    map[i, j] = 0;
+
+            return map;
+        }
+    }
+}
