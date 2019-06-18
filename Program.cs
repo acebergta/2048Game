548071cfc5d9accb681c98198320e8da3250b017
Timur Magomedov <ser4564@gmail.com>
2019-06-18 16:50:38 +0300

    18.06.19 16:50

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Game2048
 {
@@ -11,7 +7,85 @@ namespace Game2048
         static void Main(string[] args)
         {
             int[,] map = StartGame();
-            
+            DrawMap(map);
+            var input = Console.ReadKey();
+            do
+            {
+                input = Console.ReadKey();
+                switch (input.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        MoveUp(map);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        MoveDown(map);
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        MoveLeft(map);
+                        break;
+                    case ConsoleKey.RightArrow:
+                        MoveRight(map);
+                        break;
+                }
+                DrawMap(map);
+            } while (input.Key != ConsoleKey.Escape);
+        }
+
+        private static void MoveRight(int[,] map)
+        {
+
+        }
+
+        private static void MoveLeft(int[,] map)
+        {
+
+        }
+
+        private static void MoveDown(int[,] map)
+        {
+
+        }
+
+        private static void MoveUp(int[,] map)
+        {
+            for (int i = 1; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (map[i, j] != 0 && (map[i - 1, j] == 0 || map[i - 1, j] == map[i, j]))
+                    {
+                        if (map[i - 1, j] == map[i, j]) // the same
+                        {
+                            map[i - 1, j] = map[i, j] * 2;
+                            map[i, j] = 0;
+                        }
+                        else // 0
+                        {
+                            map[i - 1, j] = map[i, j];
+                            map[i, j] = 0;
+                        }
+                    }
+                }
+            }
+            AddRandNum(map);
+        }
+
+        private static void AddRandNum(int[,] map)
+        {
+
+        }
+
+        private static void DrawMap(int[,] map)
+        {
+            Console.Clear();
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(map[i, j]);
+                }
+                Console.WriteLine();
+            }
         }
 
         private static int[,] StartGame()
@@ -20,8 +94,23 @@ namespace Game2048
             for (int i = 0; i < 4; i++)
                 for (int j = 0; j < 4; j++)
                     map[i, j] = 0;
-
+            Random random = new Random();
+            for (int i = 0; i < 2; i++)
+            {
+                while (true)
+                {
+                    int xPos = random.Next(0, 4);
+                    int yPos = random.Next(0, 4);
+                    int num = random.Next(1, 2) * 2;
+                    if (map[xPos, yPos] == 0)
+                    {
+                        map[xPos, yPos] = num;
+                        break;
+                    }
+                }
+            }
             return map;
         }
+
     }
 }
