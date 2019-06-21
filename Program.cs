36be81b6c963254cc4b3d834923a6fd454ef060e
Timur Magomedov <ser4564@gmail.com>
2019-06-21 16:19:05 +0300

    MovingUpDown

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace Game2048
         {
             int[,] map = StartGame();
             DrawMap(map);
-            var input = Console.ReadKey();
+            ConsoleKeyInfo input;
             do
             {
                 input = Console.ReadKey();
@@ -38,41 +38,122 @@ namespace Game2048
 
         private static void MoveLeft(int[,] map)
         {
-
+            bool Active = false;
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 4; j++)
+                {
+                    if (map[i, j] != 0)
+                    {
+                        int yPos = i;
+                        while (yPos != 3)
+                        {
+                            if (map[yPos + 1, j] == map[yPos, j])
+                            {
+                                map[yPos + 1, j] *= 2;
+                                map[yPos, j] = 0;
+                                Active = true;
+                            }
+                            else if (map[yPos + 1, j] == 0)
+                            {
+                                map[yPos + 1, j] = map[yPos, j];
+                                map[yPos, j] = 0;
+                                Active = true;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                            yPos++;
+                        }
+                    }
+                }
+            if (Active)
+                AddRandNum(map);
         }
 
         private static void MoveDown(int[,] map)
         {
-
+            bool Active = false;
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 4; j++)
+                {
+                    if (map[i, j] != 0)
+                    {
+                        int yPos = i;
+                        while (yPos != 3)
+                        {
+                            if (map[yPos + 1, j] == map[yPos, j])
+                            {
+                                map[yPos + 1, j] *= 2;
+                                map[yPos, j] = 0;
+                                Active = true;
+                            }
+                            else if (map[yPos + 1, j] == 0)
+                            {
+                                map[yPos + 1, j] = map[yPos, j];
+                                map[yPos, j] = 0;
+                                Active = true;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                            yPos++;
+                        }
+                    }
+                }
+            if (Active)
+                AddRandNum(map);
         }
 
         private static void MoveUp(int[,] map)
         {
+            bool Active = false;
             for (int i = 1; i < 4; i++)
-            {
                 for (int j = 0; j < 4; j++)
                 {
-                    if (map[i, j] != 0 && (map[i - 1, j] == 0 || map[i - 1, j] == map[i, j]))
+                    if (map[i, j] != 0)
                     {
-                        if (map[i - 1, j] == map[i, j]) // the same
-                        {
-                            map[i - 1, j] = map[i, j] * 2;
-                            map[i, j] = 0;
-                        }
-                        else // 0
-                        {
-                            map[i - 1, j] = map[i, j];
-                            map[i, j] = 0;
+                        int yPos = i;
+                        while (yPos != 0) {
+                            if (map[yPos - 1, j] == map[yPos, j])
+                            {
+                                map[yPos - 1, j] *= 2;
+                                map[yPos, j] = 0;
+                                Active = true;
+                            }
+                            else if (map[yPos - 1, j] == 0)
+                            {
+                                map[yPos - 1, j] = map[yPos, j];
+                                map[yPos, j] = 0;
+                                Active = true;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                            yPos--;
                         }
                     }
                 }
-            }
-            AddRandNum(map);
+            if (Active)
+                AddRandNum(map);
         }
 
         private static void AddRandNum(int[,] map)
         {
-
+            Random random = new Random();
+            while (true)
+            {
+                int xPos = random.Next(0, 4);
+                int yPos = random.Next(0, 4);
+                int num = random.Next(1, 2) * 2;
+                if (map[xPos, yPos] == 0)
+                {
+                    map[xPos, yPos] = num;
+                    break;
+                }
+            }
         }
 
         private static void DrawMap(int[,] map)
